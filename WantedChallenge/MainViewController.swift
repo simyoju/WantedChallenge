310b29689cd6e6ee6ab6e8ce93bd449571b585a0
simyo <simyo@newlinkcorp.co.kr>
2023-02-27 22:22:35 +0900

    [Docs] SceneDelegate에 MainViewController 설정

--- WantedChallenge/MainViewController.swift
+++ WantedChallenge/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  WantedChallenge
 //
 //  Created by 뉴링크 on 2023/02/27.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
 
 
