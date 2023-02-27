05244c49ca2ef4da57b2edeb08eb4fbbdb02f943
simyo <simyo@newlinkcorp.co.kr>
2023-02-27 22:46:13 +0900

    [Feat] 이미지 뷰 크기 설정

--- WantedChallenge/MainViewController.swift
+++ WantedChallenge/MainViewController.swift
@@ -7,14 +7,47 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class MainViewController: UIViewController {
+    // MARK: - UI
+    let firstImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "photo")
+    }
+    
+//    let firstProgressBar = 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .yellow
+        setupView()
+        setupLayout()
     }
+}
 
-
+extension MainViewController {
+    func setupView(){
+        [
+            firstImageView
+        ].forEach{
+            view.addSubview($0)
+        }
+    }
+    
+    func setupLayout(){
+        let screenWidth = UIScreen.main.bounds.width
+        let imageViewWidth = screenWidth * 0.3
+        
+        firstImageView.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+                .offset(8)
+            $0.leading.equalToSuperview()
+                .offset(24)
+            $0.width.equalTo(imageViewWidth)
+            $0.height.equalTo(imageViewWidth * 0.75)
+        }
+    }
 }
 
